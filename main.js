2452f6316d25dea2e66c1e2735cdccb26c7e895b
LeMinhTuong <leminhtuong09122002@gmail.com>
2023-06-10 16:02:01 +0700

    new commit

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
+// Increase number in circle
 document.querySelectorAll(".increase-number").forEach(function (element) {
   var counter = 0;
   var targetValue = parseInt(element.textContent);
+  var duration = 1500;
+  var steps = 100;
+
+  var increment = (targetValue / duration) * (1000 / steps);
 
   var animation = setInterval(function () {
-    counter++;
+    counter += increment;
     if (counter >= targetValue) {
+      counter = targetValue;
       clearInterval(animation);
     }
 
-    element.textContent = counter;
-  }, 500 / targetValue);
+    element.textContent = Math.round(counter);
+  }, duration / steps);
 });
+
+// Rotate circle border
 anime({
   targets: ".border-circle",
   duration: 5000,
@@ -18,7 +26,3 @@ anime({
   easing: "easeOutCubic",
   loop: true,
 });
-
-
-const listItems = document.querySelectorAll('.desc-title_2 li');
- console.log(listItems)
